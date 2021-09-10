eebf031ec12649fc0845256c5419984dc3946417
GFT\fnpa <franklinpr2010@gmail.com>
2021-09-10 08:19:10 -0300

    primeira mensagem activemeQ

---
+++ src/main/java/com/jms/TesteConsumidor.java
@@ -0,0 +1,38 @@
+package com.jms;
+
+
+
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class TesteConsumidor {
+	public static void main(String[] args) throws NamingException, JMSException {
+		InitialContext context = new InitialContext();
+		ConnectionFactory factory = (ConnectionFactory)context.lookup("ConnectionFactory");
+	    Connection createConnection = factory.createConnection();
+	    createConnection.start();
+	    
+	    
+	    Session session = createConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+	    Destination file = (Destination)context.lookup("financeiro");
+		MessageConsumer consumer = session.createConsumer(file);
+	    
+		Message receive = consumer.receive();
+		
+		System.out.println("Recebendo message: " + receive);
+	    
+	    new Scanner(System.in).nextLine();
+	    session.close();
+	    createConnection.close();
+	    context.close();
+	}
+}
